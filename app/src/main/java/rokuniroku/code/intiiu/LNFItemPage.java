d417b8b4757a43dcb1c8079a2808520f65a52c58
Stitchy626 <coderokuniroku@gmail.com>
2018-08-04 16:48:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/rokuniroku/code/intiiu/LNFItemPage.java
+++ app/src/main/java/rokuniroku/code/intiiu/LNFItemPage.java
@@ -1,12 +1,11 @@
 package rokuniroku.code.intiiu;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,7 +15,6 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class LNFItemPage extends AppCompatActivity {
     ListView listView;
@@ -27,11 +25,15 @@ public class LNFItemPage extends AppCompatActivity {
     ArrayAdapter <String> adapter;
     LNFItem item;
     Query query;
+    TextView barView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lnfitem_page);
         item = new LNFItem();
+        barView = (TextView) findViewById(R.id.Bar) ;
         listView = (ListView) findViewById(R.id.listView);
         list = new ArrayList<>();
         adapter = new ArrayAdapter<String>(this, R.layout.lnf_item,R.id.Date, list);
@@ -39,24 +41,28 @@ public class LNFItemPage extends AppCompatActivity {
         //Get Intent from LNFPage activity
         String value = (String) getIntent().getExtras().get("cat");
 
+        if(value.equalsIgnoreCase("Student ID")){
+            barView.setText(" Date Found       Time Found         ID Number");
+        }
+
         //Database
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("LostNFound");
         rootDatabase = database.getInstance().getReference().child("LostNFound");
         query = rootDatabase.orderByChild("category").equalTo(value);
-        query.addValueEventListener(new ValueEventListener() {
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String v = (String) getIntent().getExtras().get("cat");
                 if(v.equalsIgnoreCase("Student ID")) {
                     for (DataSnapshot ds : dataSnapshot.getChildren()) {
                             item = ds.getValue(LNFItem.class);
-                            list.add(item.getFoundDate() + "       " + item.getFoundTime() + "       " + item.getDescription().toString());
+                            list.add(item.getFoundDate() + "        " + item.getFoundTime() + "             " + item.getDescription().toString());
                     }
                 }else{
                     for (DataSnapshot ds : dataSnapshot.getChildren()) {
                         item = ds.getValue(LNFItem.class);
-                        list.add(item.getFoundDate() + "       " + item.getFoundTime() + "       " + item.getVenue().toString());
+                        list.add(item.getFoundDate() + "        " + item.getFoundTime() + "             " + item.getVenue().toString());
                     }
                 }
                 listView.setAdapter(adapter);
